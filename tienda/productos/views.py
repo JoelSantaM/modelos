de3fce105d98fd1ea561240169f4483a8e9705e6
JoelSantaM <joel.santa0199@gmail.com>
2024-08-15 20:44:26 -0500

    agregar productos y paginador

--- tienda/productos/views.py
+++ tienda/productos/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Cliente
 from .models import Producto
-from .forms import ProductoModelForm
+from .forms import ProductoModelForm, ClienteModelForm
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -10,7 +12,20 @@ def listar_clientes(request):
     return render(request, 'listar_clientes.html', {'clientes': clientes})
 
 def listar_productos(request):
-    productos = Producto.objects.all()
+    query = request.GET.get('q')
+    productos_list = Producto.objects.all()
+    paginator = Paginator(productos_list,10)
+    precio_min = request.GET.get('precio_min')
+    precio_max = request.GET.get('precio_max')
+    page_number = request.GET.get('page')
+    productos = paginator.get_page('page_number')
+
+    if query:
+        productos_list = productos_list.filter(Q(nombre__icontains=query)| Q(descripcion__icontains=query))
+
+    if precio_min and precio_max:
+        productos_list = productos_list.filter(precio__gte=precio_min, precio__lte=precio_max)
+    
     return render(request, 'listar_productos.html', {'productos': productos})
 
 def inicio(request):
@@ -25,3 +40,13 @@ def agregar_producto(request):
     else:
         form = ProductoModelForm
         return render(request, 'agregar_producto.html', {'form' : form})
+
+def agregar_cliente(request):
+    if request.method == 'POST':
+        form = ClienteModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_clientes')
+    else:
+        form = ClienteModelForm()
+    return render(request, 'agregar_cliente.html', { 'form': form })
